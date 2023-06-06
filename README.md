d3ce757b45863f0d6085b604c862fee08a848730
Nequeo2000 <sonnenberg-patrick@web.de>
2023-06-06 21:14:19 +0200

    Added new activation functions

--- README.md
+++ README.md
@@ -7,18 +7,26 @@ A simple python implementation of a deep neural network.
 
 ## Activation Functions
 - [Sigmoid Function](https://en.wikipedia.org/wiki/Sigmoid_function)
+- [tanh](https://en.wikipedia.org/wiki/Hyperbolic_functions)
+- [ReLu](https://en.wikipedia.org/wiki/Rectifier_(neural_networks))
+- [Leaky Relu](https://en.wikipedia.org/wiki/Rectifier_(neural_networks))
+- [softmax](https://en.wikipedia.org/wiki/Softmax_function)
 
 ## Example Code
 ```Python
-nn = DeepNeuralNetwork(nodes=[2,5,3,1], learningrate=0.1)
+nn = DeepNeuralNetwork(nodes=[2,5,3,2],
+                        learningrate=0.1,
+                        activations=[activationfunction.Sigmoid,
+                                     activationfunction.ReLu,
+                                     activationfunction.Softmax])
 
 # training the network
-nn.fit([0,1],[1])
+nn.fit([0,1],[1,0])
 
 # get prediction from network
 print( nn.predict([0,1]) )
 ```
 ### Example Output
 ```Shell
-[0.5542525]
+[0.86699622 0.13300378]
 ```
--- deepneuralnetwork.py
+++ deepneuralnetwork.py
@@ -1,16 +1,16 @@
 import numpy
-import scipy.special
 import math
+import activationfunction
 
 class DeepNeuralNetwork:
-    def __init__(self,nodes,learningrate=None):
+    def __init__(self,nodes,learningrate=None,activations=None):
         self.lr = 1/math.sqrt(max(nodes)) if learningrate==None else learningrate
         self.weights = []
         for i in range(len(nodes)-1):
             layerWeights = numpy.random.uniform(-1,1,size=(nodes[i]+1,nodes[i+1]))
             self.weights.append(layerWeights)
         
-        self.activationFunction = lambda x: scipy.special.expit(x)
+        self.activationfunctions = (len(nodes)-2)*[activationfunction.Sigmoid]+[activationfunction.Softmax] if activations==None else activations
 
     @staticmethod
     def __addBias(array):
@@ -24,11 +24,11 @@ class DeepNeuralNetwork:
         input = self.__addBias(input)
         
         output = numpy.matmul(input,self.weights[0])
-        output = self.activationFunction(output)
+        output = self.activationfunctions[0].calc(output)
         output = self.__addBias(output)
         for i in range(1,len(self.weights)):            
             output = numpy.matmul(output,self.weights[i])
-            output = self.activationFunction(output)
+            output = self.activationfunctions[i].calc(output)
             output = self.__addBias(output) if i < len(self.weights)-1 else output
 
         return output.flatten()
@@ -40,12 +40,12 @@ class DeepNeuralNetwork:
         # calculate output of each layer
         outputs = []
         output = numpy.matmul(input,self.weights[0])
-        output = self.activationFunction(output)
+        output = self.activationfunctions[0].calc(output)
         output = self.__addBias(output)
         outputs.append(output)
         for i in range(1,len(self.weights)):
             output = numpy.matmul(output,self.weights[i])
-            output = self.activationFunction(output)
+            output = self.activationfunctions[i].calc(output)
             output = self.__addBias(output) if i < len(self.weights)-1 else output
             outputs.append(output)
         
@@ -61,13 +61,27 @@ class DeepNeuralNetwork:
         errors.reverse()
 
         # apply errors to weights
-        gradient = outputs[0][0][0]*(1-outputs[0][0][0])
         for i in range(len(self.weights)-1,0,-1):
+            gradient = self.activationfunctions[i].getStochasticGradient(outputs[i])
             if i < len(self.weights)-1:
                 error = numpy.array( errors[i].flatten().tolist()[0:-1] ,ndmin=2)
             else: error = errors[i]
             alpha = self.lr*error*gradient
-            self.weights[i] = self.weights[i]+numpy.matmul(outputs[i-1].T,alpha)
+            self.weights[i] += numpy.matmul(outputs[i-1].T,alpha)
+        gradient = self.activationfunctions[0].getStochasticGradient(outputs[0])
         error = numpy.array( errors[0].flatten().tolist()[0:-1] ,ndmin=2)
         alpha = self.lr*error*gradient
-        self.weights[0] = self.weights[0]+numpy.matmul(input.T,alpha)
+        self.weights[0] += numpy.matmul(input.T,alpha)
+
+
+nn = DeepNeuralNetwork(nodes=[2,5,3,2],
+                        learningrate=0.1,
+                        activations=[activationfunction.Sigmoid,
+                                     activationfunction.ReLu,
+                                     activationfunction.Softmax])
+
+# training the network
+nn.fit([0,1],[1,0])
+
+# get prediction from network
+print( nn.predict([0,1]) )
